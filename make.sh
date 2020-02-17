708539f88ad68e7687be2b7492161300652d92b9
Mark Raynsford <code@io7m.com>
2020-02-17 13:19:16 +0000

    Add introductory page

--- make.sh
+++ make.sh
@@ -15,6 +15,7 @@ do
   mkdir -p "out/${dir}" || fatal "Could not create output directory"
 done
 
+cp src/main/css/*.css out/ || fatal "Could not copy CSS"
 cp src/main/image/*.png out/image || fatal "Could not copy images"
 rsync -av src/main/xml/ out/ || fatal "Could not copy feeds"
 rsync -av src/main/epub out/ || fatal "Could not copy epubs"
