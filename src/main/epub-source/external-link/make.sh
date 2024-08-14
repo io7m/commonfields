17c0822132bd1ea7daf03eea71fec168c71f046f
Mark Raynsford <code@io7m.com>
2024-08-14 11:40:28 +0000

    Add epub with external link.

---
+++ src/main/epub-source/external-link/make.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -ex
+
+EPUB_NAME="external-link.epub"
+EPUB_NAME_DOTTED="../${EPUB_NAME}"
+
+rm -f "${EPUB_NAME}"
+pushd data
+zip -X -0 "${EPUB_NAME_DOTTED}" mimetype
+zip -X -u "${EPUB_NAME_DOTTED}" META-INF/container.xml
+zip -X -u "${EPUB_NAME_DOTTED}" epub/content.opf
+zip -X -u "${EPUB_NAME_DOTTED}" epub/toc.ncx
+zip -X -u "${EPUB_NAME_DOTTED}" epub/toc.xhtml
+zip -X -u "${EPUB_NAME_DOTTED}" epub/title.xhtml
+epubcheck "${EPUB_NAME_DOTTED}"
+
